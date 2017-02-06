c0149d827ba4b7a0f1bf4fe3779c4fbf6a862748
RealTimeCom <expertphp@gmail.com>
2017-02-06 23:32:18 +0200

    0.0.1

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 /* SOURCE FILE - Copyright (c) 2017 diradmin - Tanase Laurentiu Iulian - https://github.com/RealTimeCom/diradmin */
 'use strict';
+
+const http = require('fast-stream'),
+    path = require('path');
+
+function conf(opt) {
+    console.log('opt', typeof opt);
+    const s = 'src' in opt ? opt.src + '' : 'src',
+        r = {},
+        h = {
+            404: cb => cb(opt.cache && '404.html' in opt.cache ? opt.cache['404.html'] : { src: s + path.sep + '404.html' }, null, 404),
+            GET: {
+                '/': cb => cb(opt.cache && 'index.html' in opt.cache ? opt.cache['index.html'] : { src: s + path.sep + 'index.html' }),
+                '/favicon.ico': cb => cb(opt.cache && 'favicon.ico' in opt.cache ? opt.cache['favicon.ico'] : { src: s + path.sep + 'favicon.ico' }, { 'Content-Type': http.type['ico'] }),
+                '/admin.js': cb => cb(opt.cache && 'favicon.ico' in opt.cache ? opt.cache['admin.js'] : { src: s + path.sep + 'admin.js' }, { 'Content-Type': http.type['js'] }),
+            }
+        };
+    r['host' in opt ? opt.host + '' : '*'] = h;
+    return r;
+}
+
+class admin extends http {
+    constructor(db, opt) {
+        if (typeof db !== 'object') { throw new Error('invalid database type'); }
+        if (!(typeof db.mkdir === 'function' && typeof db.put === 'function')) { throw new Error('invalid database object'); }
+        if (typeof opt !== 'object') { opt = {}; }
+        super(conf(opt), opt);
+        this.db = db;
+    }
+}
+
+module.exports = admin;
--- test.js
+++ test.js
@@ -1,2 +1,58 @@
 /* TEST FILE - Copyright (c) 2017 diradmin - Tanase Laurentiu Iulian - https://github.com/RealTimeCom/diradmin */
 'use strict';
+
+const admin = require('./index.js'),
+    net = require('net'), // or tls
+    dirdb = require('dirdb'),
+    sep = require('path').sep,
+    fs = require('fs');
+
+// client.js:
+const src = 'src'; // source path directory
+const opt = { // optional, admin config
+    host: '*',
+    src: src,
+    cache: {
+        '404.html': fs.readFileSync(src + sep + '404.html'),
+        'index.html': fs.readFileSync(src + sep + 'index.html'),
+        'favicon.ico': fs.readFileSync(src + sep + 'favicon.ico'),
+        'admin.js': fs.readFileSync(src + sep + 'admin.js')
+    }
+};
+function httpAdmin(obj) { // obj: db | db.client()
+    net.createServer(socket => { // or tls
+        socket.pipe(new admin(obj, opt)).pipe(socket);
+    }).listen(80, function() {
+        console.log('HTTP server start', this.address());
+    });
+}
+
+// server.js:
+const root = __dirname + sep + 'test';
+fs.mkdirSync(root); // make a test directory
+
+const db = new dirdb(root);
+
+httpAdmin(db);
+
+/* Stream
+const client = db.client();
+client.pipe(db.server()).pipe(client);
+httpAdmin(client);
+*/
+/* Socket Stream
+const server = db.server();
+const client = db.client();
+net.createServer(socket => { // or tls
+    socket.pipe(server).pipe(socket);
+}).listen('dirdb.sock', function() { // socket file | port
+    const a = this.address();
+    console.log('DB server start', a);
+    // client.js:
+    net.connect(a.port, a.address, function() { // or tls
+        this.pipe(client).pipe(this);
+        console.log('DB client start');
+        httpAdmin(client);
+    }).once('end', () => console.log('DB client disconnected'));
+}).once('close', () => console.log('DB server close'));
+*/
